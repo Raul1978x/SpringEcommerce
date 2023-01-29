1b5a1631cdfecd3bef219f539ff0de8e936fac96
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2023-01-29 20:24:30 -0300

    mostrar informacion del producto en la pagina productoHome

--- src/main/java/com/my/ecommerce/spring/controladores/HomeControlador.java
+++ src/main/java/com/my/ecommerce/spring/controladores/HomeControlador.java
@@ -1,10 +1,14 @@
 package com.my.ecommerce.spring.controladores;
 
+import com.my.ecommerce.spring.entidades.Producto;
 import com.my.ecommerce.spring.servicio.ProductoServicio;
+import java.util.Optional;
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeControlador {
 
+    private final Logger log = LoggerFactory.getLogger(HomeControlador.class);
+            
+    
     @Autowired
     private ProductoServicio productoServicio;
 
@@ -25,4 +32,16 @@ public class HomeControlador {
 
         return "usuario/home";
     }
+    
+    @GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id producto enviado como parámetro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoServicio.get(id);
+		producto = productoOptional.get();
+
+		model.addAttribute("producto", producto);
+
+		return "usuario/productohome";
+	}
 }
