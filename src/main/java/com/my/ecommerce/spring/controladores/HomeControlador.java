1c68313bb06203016070f190448677fd3a39b2da
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2023-01-29 20:39:02 -0300

    mostrar informacion del producto en la pagina productoHome

--- src/main/java/com/my/ecommerce/spring/controladores/HomeControlador.java
+++ src/main/java/com/my/ecommerce/spring/controladores/HomeControlador.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -44,4 +45,10 @@ public class HomeControlador {
 
 		return "usuario/productohome";
 	}
+        
+        @PostMapping("/cart")
+        public String addCard(){
+            
+            return "usuario/carrito";
+        }
 }
