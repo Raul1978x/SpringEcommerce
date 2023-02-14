3340eca90f75baaf4d0e6a495efeedd55de2debd
Raul1978x <108808330+Raul1978x@users.noreply.github.com>
2023-02-14 20:00:24 -0300

    nuevo commit

--- src/main/java/com/my/ecommerce/spring/controladores/AdministradorControlador.java
+++ src/main/java/com/my/ecommerce/spring/controladores/AdministradorControlador.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.my.ecommerce.spring.servicio.IProductoServicio;
 
 /**
- *
+ * esta clase esta bien
  * @author Raúl Gómez
  */
 
